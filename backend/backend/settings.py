36a0094bf73066410dc18971f449c426f372c27c
Artemii Tretiakov <artemii.tretiakov@gmail.com>
2024-11-04 18:52:33 +0300

    allowed hosts list edited

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.153.136', 'https://dolbonos.ddns.net/', 'localhost']
+ALLOWED_HOSTS = ['84.201.153.136', 'dolbonos.ddns.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
